396cfd90c453dedd8f67a00e66bff62bee4af59d
Koki <kgoudy64@gmail.com>
2022-04-25 16:02:36 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+#add
+def add(n1, n2):
+    return n1 + n2
+
+#subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+#multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+#divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What is the first number?: "))
+num2 = int(input("What is the second number?: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick operation from the line above: ")
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
